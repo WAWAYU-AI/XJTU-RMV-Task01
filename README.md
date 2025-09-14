ed91e8fd0a346d236a5955cf0e8de15d75ad1a89
WAWAYU-AI <1942896934@qq.com>
2025-09-14 10:03:55 +0800

    第一周

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Task01
+![输出结果](./week1(1).png)
+![Ubantu安装成功](./week1(2).png)
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main() {
+    std::cout << "hello, DXRMV" << std::endl;
+    return 0;
+}
